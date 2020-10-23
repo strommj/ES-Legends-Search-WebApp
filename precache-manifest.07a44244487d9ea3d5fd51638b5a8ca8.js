8829a5ef6c115006c3b1aae967b29501509327da
Matthew Strom <strommj20@gmail.com>
2020-10-22 23:22:13 -0700

    Updates

---
+++ precache-manifest.07a44244487d9ea3d5fd51638b5a8ca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1df2e4adc3ef4cd1b60ae619212bf4f",
+    "url": "/ES-Legends-Search-WebApp/index.html"
+  },
+  {
+    "revision": "6512af980f56db8313b6",
+    "url": "/ES-Legends-Search-WebApp/static/css/main.dba898ab.chunk.css"
+  },
+  {
+    "revision": "c4e2dc4068f2feae09e1",
+    "url": "/ES-Legends-Search-WebApp/static/js/2.6adff751.chunk.js"
+  },
+  {
+    "revision": "e5067c2807f8c6eeca3663552efd09fb",
+    "url": "/ES-Legends-Search-WebApp/static/js/2.6adff751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6512af980f56db8313b6",
+    "url": "/ES-Legends-Search-WebApp/static/js/main.495045a9.chunk.js"
+  },
+  {
+    "revision": "d099445a067fb11744b7",
+    "url": "/ES-Legends-Search-WebApp/static/js/runtime-main.33313832.js"
+  },
+  {
+    "revision": "db5d8122836517965b6e0247a3a9691c",
+    "url": "/ES-Legends-Search-WebApp/static/media/search-24px.db5d8122.svg"
+  }
+]);
